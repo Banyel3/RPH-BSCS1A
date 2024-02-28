808b2b95729df51360db7db293ccdeccbc0b1066
Mio <138186128+Banyel3@users.noreply.github.com>
2024-02-28 23:38:20 +0800

    add_eliezer test.cpp

--- test.cpp
+++ test.cpp
@@ -3,10 +3,6 @@
 using namespace std;
 
 int main(){
-    int limit = 3;
-    cout<<"Hello World!";
-    
-    for (int x = 1; x < limit; x++){
-        cout << "Earl Putotoy?!?! ";
+    cout << "Hello Eliezer";
     }
 }
